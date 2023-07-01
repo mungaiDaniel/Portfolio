84217f260c4601ba150b67e3547fdfc95dd59db9
mungaiDaniel <danitomonga@gmail.com>
2023-07-01 14:41:49 +0300

    updated cv

--- src/components/Work.js
+++ src/components/Work.js
@@ -1,9 +1,6 @@
 import React, { useState } from 'react'
-import question from '../asserts/question.jpg'
-import flat from '../asserts/flat.jpg'
 import data from '../data'
-import top from '../asserts/top.jpg'
-import { Flip, Slide, Zoom } from 'react-reveal'
+import {  Zoom } from 'react-reveal'
 import Modal from './Modal'
 const Work = () => {
 
@@ -40,56 +37,6 @@ const Work = () => {
                 )
               })
             }
-
-
-            {/* <Slide left>
-
-            <article className='portfolio_item'>
-               <div className='portfolio_item-image'>
-                     <img src={question} className='question ' alt="question" />
-
-                </div> 
-                    <h3>Stackoverflow-Lite</h3>
-                    <div className='portfolio_item-cta'>
-                    <a href="https://github.com/mungaiDaniel/stackoverflow-full" className='btn cat' target="_blank">GitHub</a>
-                    <a href="https://github.com/mungaiDaniel/stackoverflow-full" className='btn btn-warning' target="_blank">Live Demo</a>
-                    </div>
-
-
-            </article>
-            </Slide>
-            <Slide bottom>
-
-            <article className='portfolio_item'>
-               <div className='portfolio_item-image'>
-                     <img src={top} className='top' alt="question" />
-                     
-                </div> 
-                    <h3>Salon Gerente</h3>
-                    <div className='portfolio_item-cta'>
-                    <a href="https://github.com/mungaiDaniel/Salon-Appointment-UI" className='btn cat' target="_blank">GitHub</a>
-                    <a href="https://github.com/mungaiDaniel/Salon-Appointment-UI" className='btn btn-warning' target="_blank">Live Demo</a>
-                    </div>
-
-
-            </article>
-            </Slide>
-            <Slide right>
-
-            <article className='portfolio_item'>
-            <div className='portfolio_item-image'>
-            <img src={flat} className='flat' alt="question" />
-            
-                </div> 
-                <h3>  Word  Hunt</h3>
-                    <div className='portfolio_item-cta'>
-                    <a href="https://github.com/mungaiDaniel/React-dictionary" className='btn cat' target="_blank">GitHub</a>
-                    <a href="https://github.com/mungaiDaniel/React-dictionary" className='btn btn-warning' target="_blank">Live Demo</a>
-                    </div>
-
-
-                    </article>
-                  </Slide> */}
 
         </div>
     </section>
